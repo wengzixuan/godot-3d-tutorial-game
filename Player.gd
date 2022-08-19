91d11374cffdf6eb2a2ba35ce35ed5d1bdd9d1bd
wzx <1986708924@qq.com>
2022-08-19 17:34:50 +0800

    基本完成的3D游戏

--- Player.gd
+++ Player.gd
@@ -6,10 +6,23 @@ export var speed = 14
 # The downward acceleration when in the air, in meters per second squared.
 export var fall_acceleration = 75
 
+# Vertical impulse applied to the character upon jumping in meters per second.
+export var jump_impulse = 20
+
+# Vertical impulse applied to the character upon bouncing over a mob in
+# meters per second.
+export var bounce_impulse = 16
+
+# Emitted when the player was hit by a mob.
+# Put this at the top of the script.
+signal hit
+
 var velocity = Vector3.ZERO
+
 
 func _physics_process(delta):
 	var direction = Vector3.ZERO
+	
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -19,10 +32,39 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
 		
-	if direction != Vector3.ZERO:
-		direction = direction.normalized()
-	$Pivot.look_at(translation + direction, Vector3.UP)
-	velocity.x = direction.x * speed
-	velocity.z = direction.z * speed
+	# Jumping.
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y += jump_impulse
+	
 	velocity.y -= fall_acceleration * delta
 	velocity = move_and_slide(velocity, Vector3.UP)
+	
+		
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		$Pivot.look_at(translation + direction, Vector3.UP)
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+	
+	
+	for index in range(get_slide_count()):
+		# We check every collision that occurred this frame.
+		var collision = get_slide_collision(index)
+		# If we collide with a monster...
+		if collision.collider.is_in_group("mob"):
+			var mob = collision.collider
+			# ...we check that we are hitting it from above.
+			if Vector3.UP.dot(collision.normal) > 0.1:
+				# If so, we squash it and bounce.
+				mob.squash()
+				velocity.y = bounce_impulse
+
+# And this function at the bottom.
+func die():
+	emit_signal("hit")
+	queue_free()
+
+
+func _on_MobDetector_body_exited(body):
+	pass # Replace with function body.
+	die()
